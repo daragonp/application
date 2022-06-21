d367c6654da6ee79a3625af308ff94e846863a19
daragonp <daragonp@outlook.com>
2022-06-21 13:19:20 -0500

    Versión 0.1

--- conexion.php
+++ conexion.php
@@ -1,3 +1,4 @@
 <?php
     // $conectar = mysqli_connect('localhost:3306', 'daragonp', 'Saranicolle8+', 'heroku');
+    $conectar = mysqli_connect('ec2-3-224-8-189.compute-1.amazonaws.com:5432', 'mzffejxumeymwl', '63bbcc094461fe2f2bbc5bed0f41157de43f1c0e4b9d0975541a971554a82e60', 'ddqjmbsguldmt1');
  ?>

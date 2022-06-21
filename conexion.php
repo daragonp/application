3670e99b57931c46c10592fb5e21edf016dab641
daragonp <47419844+daragonp@users.noreply.github.com>
2022-06-21 13:01:52 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,3 @@
+<?php
+$conectar = mysqli_connect('ec2-3-224-8-189.compute-1.amazonaws.com', 'mzffejxumeymwl', '63bbcc094461fe2f2bbc5bed0f41157de43f1c0e4b9d0975541a971554a82e60+', 'ddqjmbsguldmt1');
+?>

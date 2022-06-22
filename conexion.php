a588466a009c75ea40b71205c9790b31dd543c96
daragonp <daragonp@outlook.com>
2022-06-21 19:47:11 -0500

    Versión 0.1

--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php
     // $conectar = mysqli_connect('localhost:3306', 'daragonp', 'Saranicolle8+', 'heroku');
-    $conectar = mysqli_connect('ec2-3-224-8-189.compute-1.amazonaws.com:5432', 'mzffejxumeymwl', '63bbcc094461fe2f2bbc5bed0f41157de43f1c0e4b9d0975541a971554a82e60', 'ddqjmbsguldmt1');
+    $conectar = mysqli_connect('exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306', 'stf4hr5kofaooobj', 'jn0rlllrhti3f2td', 'ai3sdxdblvfungl3');
+    // $conectar = mysqli_connect('ec2-3-224-8-189.compute-1.amazonaws.com:5432', 'mzffejxumeymwl', '63bbcc094461fe2f2bbc5bed0f41157de43f1c0e4b9d0975541a971554a82e60', 'ddqjmbsguldmt1');
  ?>
